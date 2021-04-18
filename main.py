336bf87c4abbd46616a535f8b2387f53a0d24e01
ddxdydz_console <Lizun57@inbox.ru>
2021-04-18 20:52:46 +0700

    Fixed bugs, deploy

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import os
 from csv import DictReader
-from requests import get
 
 from flask import Flask, render_template
 
 from data import db_session
-from data.__all_models import *
-from forms.registerform import RegisterForm
-from forms.loginform import LoginForm
-from forms.topicform import TopicForm
-from forms.questionform import QuestionForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I2D4423D2Q53D'
@@ -30,9 +24,9 @@ def show_page(page_name=''):
 
 def main():
     db_session.global_init('db/forum.db')
-    app.run(port=8080, host='127.0.0.1')
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
